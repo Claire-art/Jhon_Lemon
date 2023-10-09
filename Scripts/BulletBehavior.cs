6cf7413146116616fb15f02721e90c247a3e400b
HAN JUA <juachef@gmail.com>
2023-10-09 15:57:15 +0900

    최종 코드 백업

---
+++ Scripts/BulletBehavior.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    public GameObject explosionEffect;
+    public static int enemyCount = 0; // Add this
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "enemy") {
+            Instantiate(explosionEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+            enemyCount += 1;
+        }
+
+        Destroy(gameObject);
+    }
+}
